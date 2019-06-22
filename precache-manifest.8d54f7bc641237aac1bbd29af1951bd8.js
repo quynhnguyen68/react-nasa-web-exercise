b39dd0e9f44088958f85d6e7401c8087ea9f6e53
Quynh Nguyen <vanquynh6894@gmail.com>
2019-06-22 13:14:38 +0700

    Updates

---
+++ precache-manifest.8d54f7bc641237aac1bbd29af1951bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217e8f4dbc3bda9f8dd0a7cf9944da81",
+    "url": "/react-nasa-web-exercise/index.html"
+  },
+  {
+    "revision": "f52b25be99f0294dbdf7",
+    "url": "/react-nasa-web-exercise/static/css/main.b407c83d.chunk.css"
+  },
+  {
+    "revision": "5e5bdd5ea76f10086b2a",
+    "url": "/react-nasa-web-exercise/static/js/2.b363dde1.chunk.js"
+  },
+  {
+    "revision": "f52b25be99f0294dbdf7",
+    "url": "/react-nasa-web-exercise/static/js/main.92f416b8.chunk.js"
+  },
+  {
+    "revision": "cda3bba6aa07dffeedd4",
+    "url": "/react-nasa-web-exercise/static/js/runtime~main.6767d9d4.js"
+  }
+]);
